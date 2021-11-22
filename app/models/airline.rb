6197a6c6b05a18edd37d63af6fd1e27dfb3e79dd
Dalila <babaameur.dalila@gmail.com>
2021-11-22 12:30:53 +0100

    created and tested the contollers via insomnia

---
+++ app/models/airline.rb
@@ -0,0 +1,13 @@
+class Airline < ApplicationRecord
+  has_many :reviews
+
+  before_create :slugify
+  def slugify
+    #take the name of the airline and convert it into a url safe and set it as a slug value
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
+end
